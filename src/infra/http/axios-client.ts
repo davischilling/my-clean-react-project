22c9daad89241a38a16baf8c816e04882ee46dd4
Davi Schilling <davischilling.dev@gmail.com>
2021-10-04 21:17:07 +0200

    ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-client.ts
@@ -0,0 +1,9 @@
+import { HttpPostClient } from '@/data/contracts/http'
+
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async post (params: HttpPostClient.Request<any>): Promise<void> {
+    await axios(params.url)
+  }
+}
