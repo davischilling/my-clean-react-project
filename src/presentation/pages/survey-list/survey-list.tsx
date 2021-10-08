05ce137e0f5083aaec00614e44d7f4e9c15cfef0
Davi Schilling <davischilling.dev@gmail.com>
2021-10-08 18:06:24 +0200

    feat: ensure SurveyList Component renders 4 empty items on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Footer, Header } from '@/presentation/components'
 import Styles from './survey-list-styles.scss'
+import { SurveyItemEmpty } from './components'
 
 const SurveyList: React.FC = () => {
   return (
@@ -9,8 +10,8 @@ const SurveyList: React.FC = () => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul>
-
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty />
         </ul>
       </div>
       <Footer />
