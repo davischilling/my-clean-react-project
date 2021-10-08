77640c3b52414703b28859e05b9154806a4a8530
Davi Schilling <davischilling.dev@gmail.com>
2021-10-08 17:57:51 +0200

    refactor: move survey list items to components

---
+++ src/presentation/pages/survey-list/components/index.ts
@@ -0,0 +1,2 @@
+export { default as SurveyItem } from './survey-item/item'
+export { default as SurveyItemEmpty } from './survey-item-empty/item-empty'
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-import { Footer, Header, Icon } from '@/presentation/components'
+import { Footer, Header } from '@/presentation/components'
 import Styles from './survey-list-styles.scss'
-import { IconName } from '@/presentation/components/icon/icon'
 
 const SurveyList: React.FC = () => {
   return (
@@ -11,23 +10,7 @@ const SurveyList: React.FC = () => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>03</span>
-                <span className={Styles.year}>2021</span>
-              </time>
-              <p>Qual é o seu framework web favorito?</p>
-            </div>
-            <footer>
-              Ver resultado
-            </footer>
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
+
         </ul>
       </div>
       <Footer />
