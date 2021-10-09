0bdf24dd7179651aeb21fbfe7f37152690e92884
Davi Schilling <davischilling.dev@gmail.com>
2021-10-09 18:43:29 +0200

    feat: ensure SurveyList Page renders SurveyItems on success and error on failure

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,31 +1,60 @@
 import React from 'react'
-import { render, cleanup, RenderResult } from '@testing-library/react'
+import { render, cleanup, RenderResult, waitFor } from '@testing-library/react'
 
 import { SurveyList } from '@/presentation/pages'
 import { LoadSurveyListStub } from '@/data/test'
+import { UnexpectedError } from '@/data/error'
 
-describe('SurveyList Component', () => {
+const makeSut = (loadSurveyList: LoadSurveyListStub): RenderResult => {
+  return render(
+    <SurveyList loadSurveyList={loadSurveyList} />
+  )
+}
+
+describe('SurveyList Page', () => {
+  let loadSurveyList: LoadSurveyListStub
   let loadSurveyListSpy: jest.SpyInstance
-  let sut: RenderResult
 
   beforeEach(() => {
-    const loadSurveyList = new LoadSurveyListStub()
+    loadSurveyList = new LoadSurveyListStub()
     loadSurveyListSpy = jest.spyOn(loadSurveyList, 'loadAll')
-
-    sut = render(
-      <SurveyList loadSurveyList={loadSurveyList} />
-    )
   })
 
   afterEach(cleanup)
 
   test('should render 4 empty items on start', () => {
+    const sut = makeSut(loadSurveyList)
     const surveyList = sut.getByTestId('survey-list')
 
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    expect(sut.queryByTestId('error')).not.toBeInTheDocument()
   })
 
-  test('should call LoadSurveyList', () => {
+  test('should call LoadSurveyList', async () => {
+    const sut = makeSut(loadSurveyList)
+
     expect(loadSurveyListSpy).toHaveBeenCalledTimes(1)
+    expect(sut.queryByTestId('error')).not.toBeInTheDocument()
+    await waitFor(() => sut.getByRole('heading'))
+  })
+
+  test('should render SurveyItems on success', async () => {
+    const sut = makeSut(loadSurveyList)
+    const surveyList = sut.getByTestId('survey-list')
+
+    await waitFor(() => surveyList)
+
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3)
+  })
+
+  test('should render Error on failure', async () => {
+    const error = new UnexpectedError()
+    jest.spyOn(loadSurveyList, 'loadAll').mockRejectedValueOnce(error)
+
+    const sut = makeSut(loadSurveyList)
+    await waitFor(() => sut.getByRole('heading'))
+
+    expect(sut.queryByTestId('survey-list')).not.toBeInTheDocument()
+    expect(sut.queryByTestId('error')).toHaveTextContent(error.message)
   })
 })
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Footer, Header } from '@/presentation/components'
 import Styles from './survey-list-styles.scss'
-import { SurveyItemEmpty } from './components'
+import { SurveyItem, SurveyItemEmpty } from './components'
 import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[],
+    error: ''
+  })
+
   useEffect(() => {
-    (async function () {
-      loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll()
+      .then(surveys => setState({ ...state, surveys }))
+      .catch(error => setState({ ...state, error: error.message }))
   }, [])
 
   return (
@@ -21,9 +27,22 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          <SurveyItemEmpty />
-        </ul>
+        {
+          state.error
+            ? <div>
+              <span data-testid="error">
+                {state.error}
+              </span>
+              <button>Recarregar</button>
+            </div>
+            : <ul data-testid="survey-list">
+                {
+                  state.surveys.length
+                    ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+                    : <SurveyItemEmpty />
+                }
+              </ul>
+        }
       </div>
       <Footer />
     </div>
